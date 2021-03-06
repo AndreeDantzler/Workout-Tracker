14ccf81e8186404b76618fc081a5070d108deae5
Andree Dantzler <andree.balayssac@icloud.com>
2021-03-05 21:01:08 -0500

    deploying

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
-
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 require("./routes/apiroutes")(app);
 require("./routes/htmlroutes")(app)
 
